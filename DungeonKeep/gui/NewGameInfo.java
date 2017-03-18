b2d1b200f519a79c98f0112b24fd1ca34b7930b9
theantonioalmeida <tony-alm@hotmail.com>
2017-03-18 19:32:32 +0000

    [I#5] Fixes #2

---
+++ DungeonKeep/gui/NewGameInfo.java
@@ -0,0 +1,124 @@
+package gui;
+
+import logic.*;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import logic.DungeonLevel;
+import logic.Game;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Window.Type;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class NewGameInfo extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField ogreNumberInput;
+	private WindowKeep window;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NewGameInfo frame = new NewGameInfo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public NewGameInfo() {
+		setType(Type.POPUP);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 375, 202);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox<String> guardPersonalityChooser = new JComboBox<String>();
+		guardPersonalityChooser.setBounds(181, 48, 161, 27);
+		contentPane.add(guardPersonalityChooser);
+		guardPersonalityChooser.addItem("Rookie");
+		guardPersonalityChooser.addItem("Drunken");
+		guardPersonalityChooser.addItem("Suspicious");
+		
+		JLabel lblGuardPersonality = new JLabel("Guard Personality");
+		lblGuardPersonality.setBounds(31, 51, 130, 18);
+		lblGuardPersonality.setForeground(Color.BLACK);
+		lblGuardPersonality.setFont(new Font("Helvetica Neue", Font.PLAIN, 14));
+		contentPane.add(lblGuardPersonality);
+		
+		ogreNumberInput = new JTextField();
+		ogreNumberInput.setBounds(183, 9, 65, 27);
+		ogreNumberInput.setColumns(10);
+		contentPane.add(ogreNumberInput);
+		
+		JLabel lblNumberOfOgres = new JLabel("Number of Ogres");
+		lblNumberOfOgres.setBounds(31, 6, 110, 33);
+		lblNumberOfOgres.setForeground(Color.BLACK);
+		lblNumberOfOgres.setFont(new Font("Helvetica Neue", Font.PLAIN, 14));
+		contentPane.add(lblNumberOfOgres);
+		
+		JLabel lblGameStarterInfo = new JLabel("Please enter the game options");
+		lblGameStarterInfo.setBounds(31, 149, 311, 16);
+		contentPane.add(lblGameStarterInfo);
+		
+		JButton btnStartGame = new JButton("Start game!");
+		btnStartGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String guardType = (String) guardPersonalityChooser.getSelectedItem();
+
+				int nOgres;
+
+				try {
+					nOgres = Integer.parseInt(ogreNumberInput.getText());
+				}
+				catch(NumberFormatException ex) {
+					lblGameStarterInfo.setText("Don't be silly, enter a number!");
+					return;
+				}
+
+				if(nOgres < 1 || nOgres > 5) {
+					lblGameStarterInfo.setText("Number of ogres must be 1-5");
+					return;
+				}
+
+				window.setGame(new Game(nOgres, guardType, new DungeonLevel(guardType)));
+				lblGameStarterInfo.setText("");
+				window.requestFocus();
+				window.frame.setEnabled(true);
+				window.setStatusMessage("Press the keyboard arrows to move the hero.");
+			}
+		});
+		btnStartGame.setBounds(121, 87, 130, 50);
+		contentPane.add(btnStartGame);
+
+	}
+	
+	public NewGameInfo(WindowKeep window) {
+		this();
+		this.window = window;
+	}
+}
--- DungeonKeep/gui/WindowKeep.java
+++ DungeonKeep/gui/WindowKeep.java
@@ -20,12 +20,14 @@ import java.awt.Component;
 import java.awt.Font;
 import javax.swing.JToggleButton;
 import java.awt.BorderLayout;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
 
 public class WindowKeep {
 
-	private JFrame frame;
-	private JInternalFrame iframe;
+	protected JFrame frame;
 	protected Game game = null;
+	private JFrame newGameFrame;
 
 	/**
 	 * Launch the application.
@@ -55,107 +57,44 @@ public class WindowKeep {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+
 		frame.setResizable(false);
 		frame.getContentPane().setFocusable(false);
 		frame.getContentPane().setFont(new Font("Helvetica Neue", Font.PLAIN, 14));
 		frame.getContentPane().setBackground(Color.DARK_GRAY);
 		frame.setBackground(Color.LIGHT_GRAY);
-		frame.setBounds(100, 100, 638, 479);
+		frame.setBounds(100, 100, 590, 384);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-
-		iframe = new JInternalFrame("New Game Info Selector");
-		iframe.setBounds(111, 122, 411, 199);
-		frame.getContentPane().add(iframe);
-		iframe.getContentPane().setLayout(null);
-		iframe.setVisible(false);
-
-		JComboBox<String> guardPersonalityChooser = new JComboBox<String>();
-		guardPersonalityChooser.setBounds(176, 12, 225, 31);
-		iframe.getContentPane().add(guardPersonalityChooser);
-		guardPersonalityChooser.addItem("Rookie");
-		guardPersonalityChooser.addItem("Drunken");
-		guardPersonalityChooser.addItem("Suspicious");
-
-		JLabel lblGuardPersonality = new JLabel("Guard Personality");
-		lblGuardPersonality.setBounds(10, 19, 132, 16);
-		iframe.getContentPane().add(lblGuardPersonality);
-		lblGuardPersonality.setForeground(Color.BLACK);
-		lblGuardPersonality.setFont(new Font("Helvetica Neue", Font.PLAIN, 14));
-
-		JTextField ogreNumberInput = new JTextField();
-		ogreNumberInput.setBounds(176, 54, 127, 19);
-		iframe.getContentPane().add(ogreNumberInput);
-		ogreNumberInput.setColumns(10);
-
-		JLabel lblNumberOfOgres = new JLabel("Number of Ogres");
-		lblNumberOfOgres.setBounds(10, 55, 411, 17);
-		iframe.getContentPane().add(lblNumberOfOgres);
-		lblNumberOfOgres.setFont(new Font("Helvetica Neue", Font.PLAIN, 14));
-		lblNumberOfOgres.setForeground(Color.BLACK);
 		
-		JLabel lblGameStarterInfo = new JLabel("");
-		lblGameStarterInfo.setBounds(12, 108, 217, 47);
-		iframe.getContentPane().add(lblGameStarterInfo);
+		newGameFrame = new NewGameInfo(this);
 
 		JLabel lblStatus = new JLabel("You can start a new game.");
 		lblStatus.setName("lblStatus");
-		lblStatus.setBounds(38, 415, 357, 16);
+		lblStatus.setBounds(36, 322, 357, 16);
 		lblStatus.setForeground(Color.WHITE);
 		lblStatus.setFont(new Font("Helvetica Neue", Font.PLAIN, 12));
 		frame.getContentPane().add(lblStatus);
 
 		JPanel gamePanel = new GamePanel(this);
-		gamePanel.setBounds(26, 116, 369, 287);
+		gamePanel.setBounds(26, 23, 369, 287);
 		frame.getContentPane().add(gamePanel);
 		gamePanel.requestFocusInWindow();
-		
-		JButton btnStartGame = new JButton("Start game!");
-		btnStartGame.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String guardType = (String) guardPersonalityChooser.getSelectedItem();
-
-				int nOgres;
-
-				try {
-					nOgres = Integer.parseInt(ogreNumberInput.getText());
-				}
-				catch(NumberFormatException ex) {
-					lblGameStarterInfo.setText("Don't be silly, enter a number!");
-					return;
-				}
-
-				if(nOgres < 1 || nOgres > 5) {
-					lblGameStarterInfo.setText("Number of ogres must be 1-5");
-					return;
-				}
-
-				game = new Game(nOgres, guardType, new DungeonLevel(guardType));
-				lblGameStarterInfo.setText("");
-				iframe.setVisible(false);
-				gamePanel.repaint();
-				gamePanel.requestFocusInWindow();
-				
-				lblStatus.setText("Press the keyboard arrows to move the hero.");
-			}
-		});
-		btnStartGame.setBounds(247, 130, 142, 25);
-		iframe.getContentPane().add(btnStartGame);
 		
 		JButton btnNewGame = new JButton("New Game");
 		btnNewGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				iframe.requestFocusInWindow();
-				iframe.moveToFront();
-				iframe.setVisible(true);
+				frame.setEnabled(false);
+				newGameFrame.requestFocusInWindow();
+				newGameFrame.setVisible(true);
 			}
 		});
 
-		btnNewGame.setBounds(442, 129, 117, 29);
+		btnNewGame.setBounds(442, 60, 117, 29);
 		frame.getContentPane().add(btnNewGame);
 
 		JButton btnNewBuexittton = new JButton("Exit");
-		btnNewBuexittton.setBounds(442, 357, 117, 29);
+		btnNewBuexittton.setBounds(442, 243, 117, 29);
 		btnNewBuexittton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
@@ -163,9 +102,24 @@ public class WindowKeep {
 		});
 		frame.getContentPane().add(btnNewBuexittton);
 	}
+	
+	public void requestFocus() {
+		newGameFrame.setVisible(false);
+		frame.requestFocus();
+		frame.getContentPane().getComponent(1).requestFocus();
+	}
+	
+	public void setGame(Game game) {
+		this.game = game;
+	}
+	
+	public void setStatusMessage(String str) {
+		JLabel gameStatus = (JLabel) frame.getContentPane().getComponent(0);
+		gameStatus.setText(str);
+	}
 
 	public void finishGame(){
-		JLabel gameStatus = (JLabel) frame.getContentPane().getComponent(1);
+		JLabel gameStatus = (JLabel) frame.getContentPane().getComponent(0);
 		gameStatus.setText(game.finalMessage());
 		game = null;
 	}
