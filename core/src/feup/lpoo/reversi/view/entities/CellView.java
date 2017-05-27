796a30f787a6cc82f9cbca5082c6e8d95431ea6c
theantonioalmeida <tony-alm@hotmail.com>
2017-05-27 15:05:11 +0100

    Implement basic version of piece rotation animation

--- core/src/feup/lpoo/reversi/view/entities/CellView.java
+++ core/src/feup/lpoo/reversi/view/entities/CellView.java
@@ -2,7 +2,10 @@ package feup.lpoo.reversi.view.entities;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
@@ -17,6 +20,18 @@ import feup.lpoo.reversi.presenter.GamePresenter;
  */
 
 public class CellView extends Actor {
+    private static Texture regionBlack = new Texture("rotation/black-rotation.png");
+    private static Texture regionWhite = new Texture("rotation/white-rotation.png");
+
+    private static TextureRegion[][] blackFramesTmp = TextureRegion.split(regionBlack, 64, 64);
+    private static TextureRegion[][] whiteFramesTmp = TextureRegion.split(regionWhite, 64, 64);
+
+    private static TextureRegion[] blackFrames = new TextureRegion[7];
+    private static TextureRegion[] whiteFrames = new TextureRegion[7];
+
+    private static Animation<TextureRegion> blackAnimation;
+    private static Animation<TextureRegion> whiteAnimation;
+
     private static final Texture tile = new Texture(Gdx.files.internal("tile.png"));
 
     private static final Texture black = new Texture(Gdx.files.internal("black.png"));
@@ -27,11 +42,19 @@ public class CellView extends Actor {
 
     private GamePresenter presenter;
 
+    private char previousPiece;
+    private int animation;
+
     static {
         ICONS.put('B', black);
         ICONS.put('W', white);
         ICONS.put('X', suggestion);
         ICONS.put('-', null);
+
+        System.arraycopy(blackFramesTmp[0], 0, blackFrames, 0, 7);
+        System.arraycopy(whiteFramesTmp[0], 0, whiteFrames, 0, 7);
+        blackAnimation = new Animation<TextureRegion>(0.09f, blackFrames);
+        whiteAnimation = new Animation<TextureRegion>(0.09f, whiteFrames);
     }
 
     private Texture getIcon(char piece) {
@@ -48,6 +71,9 @@ public class CellView extends Actor {
     private int boardX;
     private int boardY;
 
+    private float stateTime = 0;
+    private boolean isBlack;
+
     public CellView(GamePresenter presenter, int x, int y, int boardX, int boardY) {
         this.presenter = presenter;
         actorX = x;
@@ -55,8 +81,7 @@ public class CellView extends Actor {
         this.boardX = boardX;
         this.boardY = boardY;
         setBounds(actorX, actorY, tile.getWidth(), tile.getHeight());
-
-        act(0);
+        previousPiece = presenter.getCurrentPiece(boardX, boardY);
         addListeners();
     }
 
@@ -76,14 +101,45 @@ public class CellView extends Actor {
     @Override
     public void act(float dt) {
         char currentPiece = presenter.getCurrentPiece(boardX, boardY);
+
+        if(previousPiece == 'B' && currentPiece == 'W') {
+            animation = 1;
+        }
+        else if(previousPiece == 'W' && currentPiece == 'B') {
+            animation = 2;
+        }
+
+        if(animation != 0)
+            stateTime += dt;
+
         icon = getIcon(currentPiece);
+        previousPiece = currentPiece;
     }
 
     @Override
     public void draw(Batch batch, float alpha) {
         batch.draw(tile,actorX,actorY);
 
+        // Get current frame of animation for the current stateTime
+       // TextureRegion currentFrame = blackAnimation.getKeyFrame(stateTime, true);
+        TextureRegion currentFrame = null;
+
         if(icon != null)
             batch.draw(icon, actorX, actorY);
+
+        if(animation == 1)
+            currentFrame = blackAnimation.getKeyFrame(stateTime, true);
+        else if(animation == 2)
+            currentFrame = whiteAnimation.getKeyFrame(stateTime, true);
+
+        if(stateTime > 0.63) {
+            stateTime = 0;
+            animation = 0;
+            currentFrame = null;
+        }
+
+        if(currentFrame != null)
+            batch.draw(currentFrame, actorX, actorY);
+
     }
 }
